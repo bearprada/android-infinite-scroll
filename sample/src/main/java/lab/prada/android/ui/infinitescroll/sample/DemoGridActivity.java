a63395106211c4bc7d6dd651706de54edfb5b0e3
prada <prada@PradaMacBook-Pro.local>
2026-09-12 12:26:59 +0100

    Remove deprecated sample activity APIs

--- sample/src/main/java/lab/prada/android/ui/infinitescroll/sample/DemoGridActivity.java
+++ sample/src/main/java/lab/prada/android/ui/infinitescroll/sample/DemoGridActivity.java
@@ -3,7 +3,7 @@ package lab.prada.android.ui.infinitescroll.sample;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.Menu;
+import android.os.Looper;
 import android.widget.GridView;
 
 import lab.prada.android.ui.lab.prada.android.ui.infinitescroll.InfiniteScrollAdapter;
@@ -16,24 +16,17 @@ public class DemoGridActivity extends Activity implements InfiniteScrollAdapter.
     private GridView mGridView;
     private InfiniteScrollAdapter<SampleAdapter> mAdapter;
     private Handler mHandler;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo_grid);
-        mGridView = (GridView)findViewById(R.id.gridView1);
+        mGridView = (GridView) findViewById(R.id.gridView1);
         mAdapter = new InfiniteScrollAdapter<SampleAdapter>(this,
                 new SampleAdapter(this), GRID_ITEM_WIDTH, GRID_ITEM_HEIGHT);
         mAdapter.addListener(this);
         mGridView.setAdapter(mAdapter);
-        mHandler = new Handler();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.demo_grid, menu);
-        return true;
+        mHandler = new Handler(Looper.getMainLooper());
     }
 
     @Override
@@ -43,13 +36,10 @@ public class DemoGridActivity extends Activity implements InfiniteScrollAdapter.
             public void run() {
                 mAdapter.getAdapter().addCount(5);
                 mAdapter.handledRefresh();
-                // when the adapter load more then 100 items. i will disable the
-                // feature of load more.
                 if (mAdapter.getOriginalAdapter().getCount() > 100) {
                     mAdapter.canReadMore(false);
                 }
             }
         }, 3000);
     }
-
 }
--- sample/src/main/java/lab/prada/android/ui/infinitescroll/sample/DemoListActivity.java
+++ sample/src/main/java/lab/prada/android/ui/infinitescroll/sample/DemoListActivity.java
@@ -3,8 +3,8 @@ package lab.prada.android.ui.infinitescroll.sample;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.view.Gravity;
-import android.view.Menu;
 import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
@@ -24,22 +24,16 @@ public class DemoListActivity extends Activity implements InfiniteScrollAdapter.
         setContentView(R.layout.activity_demo_list);
 
         RelativeLayout progress = new RelativeLayout(this);
-        progress.setLayoutParams(new GridView.LayoutParams(GridView.LayoutParams.MATCH_PARENT, 100));
+        progress.setLayoutParams(new GridView.LayoutParams(
+                GridView.LayoutParams.MATCH_PARENT, 100));
         progress.setGravity(Gravity.CENTER);
         progress.addView(new ProgressBar(this));
 
         mAdapter = new InfiniteScrollAdapter<SampleAdapter>(this,
                 new SampleAdapter(this), progress);
         mAdapter.addListener(this);
-        mHandler = new Handler();
-
-        ((ListView)this.findViewById(R.id.listView)).setAdapter(mAdapter);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.demo_list, menu);
-        return true;
+        mHandler = new Handler(Looper.getMainLooper());
+        ((ListView) findViewById(R.id.listView)).setAdapter(mAdapter);
     }
 
     @Override
@@ -52,5 +46,4 @@ public class DemoListActivity extends Activity implements InfiniteScrollAdapter.
             }
         }, 3000);
     }
-
 }
